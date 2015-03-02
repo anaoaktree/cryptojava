74b754dcd10d6d2d84d4cb1b88aae29284fd1c86
anapmc.carvalho@gmail.com <anapmc.carvalho@gmail.com>
2015-03-02 19:24:35 +0000

    key agreement diffie-hellman

---
+++ dh/Server_dh.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.*;
+import java.net.*;
+import java.nio.file.*;
+import java.lang.String;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author User
+ */
+public class Server {
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+
+    public static void main(String[] args) throws IOException {
+	   ServerSocket ss = new ServerSocket(4567);
+	   int client_id = 0;
+
+	   while (true) {
+	     Socket cs = ss.accept();
+	     Thread t1 = new Thread(new ReadMessage(cs,client_id));
+	     t1.start();
+	     client_id++;
+	   }
+    }
+
+   
+}
+
+class ReadMessage implements Runnable {
+    
+    private Socket client;
+    private int id;
+
+    
+    public ReadMessage(Socket client,int id) {
+	   this.client = client;
+	   this.id=id;
+    }
+    public void run() {
+    	Boolean firstTime=true;
+    	String cipherMode="";
+	try {
+		if (firstTime){
+	    BufferedReader in = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
+	    String msg;
+            while ((msg = in.readLine()) != null){
+            	text=(String) msg;
+            	if(firstTime){
+
+            	}
+                firstTime=false;
+           } 
+		}
+		
+        }
+        System.out.println("["+id+"]: "+"Client disconnected");
+	} catch (Exception e) {System.out.println(e);}
+    }
+}
