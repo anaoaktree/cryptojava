0ade052ef8257c60e429aa17cf76f0d1019251e4
Fábio Fernandes <fabiofernandes@MBP-de-Fabio.lan>
2015-03-13 07:59:04 +0000

    secure channel Client

--- secure_channel/Client.java
+++ secure_channel/Client.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package secure_channel;
 
 /**
@@ -28,15 +23,13 @@ import javax.crypto.spec.*; /* SecretKeySpec; IvParameterSpec;  DHParameterSpec*
 
 
 public class Client {
+
     public static void main(String args[])
     {
+        
+        /*Initialize Supported Ciphers*/
         SupportedCiphers supportedCiphers = new SupportedCiphers();
-        supportedCiphers.add("rc4", "RC4");
-        supportedCiphers.add("cbc","AES/CBC/NoPadding");
-        supportedCiphers.add("cfb8","AES/CFB8/NoPadding");
-        supportedCiphers.add("cfb","AES/CFB/NoPadding");
-        supportedCiphers.add("cbc_pdd","AES/CBC/PKCS5Padding");
-        supportedCiphers.add("cfb8_pdd","AES/CFB8/PKCS5Padding");
+        supportedCiphers.initialize(supportedCiphers);
 
         Socket s;
         String mode = "";
@@ -56,122 +49,68 @@ public class Client {
         try{
             s = new Socket("localhost", 4567);
             PrintWriter writer = new PrintWriter(s.getOutputStream());
-             /**
+            
+            /**
             * Sends cipher mode to server
             */
             writer.println(mode);
             writer.flush();
 
-
             DataInputStream in = new DataInputStream(s.getInputStream());
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
             
+            /**
+            * Computes DH key Agreement and sends public key to client
+            **/ 
+            Key_Agreement_DH dh_agreement= new Key_Agreement_DH();
+            dh_agreement.genParams("manual");
+            byte[] pubSelf = dh_agreement.getPublicKey().getEncoded();
+            out.writeInt(pubSelf.length);
+            out.write(pubSelf);
 
-             /**
-        * Computes DH key Agreement and sends public key to client
-        **/ 
-        Key_Agreement_DH dh_agreement= new Key_Agreement_DH();
-        dh_agreement.genParams("manual");
-        byte[] pubSelf = dh_agreement.getPublicKey().getEncoded();
-        out.writeInt(pubSelf.length);
-        out.write(pubSelf);
-        /**
-        * Gets public key from server
-        **/
-        byte[] pubServer = new byte[in.readInt()];
-        in.readFully(pubServer);
+            /**
+            * Gets public key from server
+            **/
+            byte[] pubServer = new byte[in.readInt()];
+            in.readFully(pubServer);
 
-        /**
-        *
-        *ka.generateSecret() generates the shared secret between two parties
-        */
-       byte[] sessionKeyBytes = dh_agreement.keyAgreement(pubServer);
-        MessageDigest sha = MessageDigest.getInstance("SHA-1");
-        sessionKeyBytes=sha.digest(sessionKeyBytes);
-        sessionKeyBytes=Arrays.copyOf(sessionKeyBytes,16);
-
+            /**
+            *
+            *ka.generateSecret() generates the shared secret between two parties
+            */
+            byte[] sessionKeyBytes = dh_agreement.keyAgreement(pubServer);
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            sessionKeyBytes=sha.digest(sessionKeyBytes);
+            sessionKeyBytes=Arrays.copyOf(sessionKeyBytes,16);
 
             /**
             * Creates a new cipher from key keyfile
+            * Getting IV from Server
             **/
-            Encrypt enc = new Encrypt();
-            Cipher cipher = enc.encrypt(mode,sessionKeyBytes);
+            byte[] iv = new byte[16];
+            in.readFully(iv);
 
+            Encrypt enc = new Encrypt();
+            Cipher cipher = enc.encrypt(mode,sessionKeyBytes,iv);
 
             OutputStream os = s.getOutputStream();
             CipherOutputStream cos = new CipherOutputStream(os,cipher); 
 
             System.out.println("Connected to server");
             System.out.println("Mode: "+mode);                
-            
-             
-            while((test=System.in.read())!=-1)
-            {
+            System.out.println("You can start typing now!\n");
+                
+            while((test=System.in.read())!=-1){
                 cos.write((byte)test);
                 cos.flush();
             }
             in.close();
             out.close();
-            s.close();
-                       
-        }
-        catch(Exception e)
-        {
+            s.close();             
+        }catch(Exception e){
             System.out.println("*** Failed to connect to server ***");
         }
     }
 }
-   
-class Encrypt{
-    public Cipher encrypt(String mode,byte [] keyfile){
-        try{
-            System.out.println("enc "+mode);
-            Cipher cipher = Cipher.getInstance(mode);
-            //IV
-            byte[] iv = new byte[16];
-            SecureRandom sr = new SecureRandom();
-            sr.nextBytes(iv);
-
-            
-            SecretKey key;
-            if(mode.startsWith("AES")){
-                key = new SecretKeySpec(keyfile,"AES");    
-                cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(iv));
-            }
-            else{
-                key = new SecretKeySpec(keyfile,"RC4");
-                cipher.init(Cipher.ENCRYPT_MODE, key);
-            }
-            return cipher;
-        }
-        catch (Exception e) {System.out.println(e);}
-        return null;
-    }
-}
 
 
-class SupportedCiphers{
-    private HashMap<String, String> ciphers;
-    
-    public SupportedCiphers(){
-        this.ciphers = new HashMap<String, String>();
-    }
-
-    public void add(String cod, String cipher){
-        this.ciphers.put(cod, cipher);
-    }
-
-    public String getCipher(String cod){
-        if (this.ciphers.containsKey(cod)){
-            return this.ciphers.get(cod);
-        }
-        return "";
-    }
-    public String getSupportedCiphers(){
-        String res = "";
-        for(String s : this.ciphers.keySet()){
-            res = res + "Identifier "+s+"  Cipher: "+this.ciphers.get(s)+"\n";
-        }
-        return res;
-    }
-}
